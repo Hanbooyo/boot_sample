9e73b46936177b605a3eba17ccd77ae200153a9f
Hanbooyo <qnfudk@gmail.com>
2023-04-22 13:24:44 +0900

    JWT 설정, kakao Auth 테스트

---
+++ src/main/java/com/app/domain/member/entity/Member.java
@@ -0,0 +1,46 @@
+package com.app.domain.member.entity;
+
+import com.app.domain.common.BaseEntity;
+import com.app.domain.member.constant.MemberType;
+import com.app.domain.member.constant.Role;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED) //Entity 클래스는 기본 생성자가 필수라서.
+public class Member extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) //기본키 생성 전략을 DB에 맡김
+    private Long memberId;
+
+    @Enumerated(EnumType.STRING) // 기본값이 Ordinary라 String으로 지정
+    @Column(nullable = false, length = 10) //length 기본값 255
+    private MemberType memberType;
+
+    @Column(unique = true, length = 50, nullable = false)
+    private String email;
+
+    @Column(length = 200)
+    private String password;
+
+    @Column(nullable = false, length = 20)
+    private String memberName;
+
+    @Column(length = 200)
+    private String profile;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false, length = 10)
+    private Role role;
+
+    @Column(length = 250)
+    private String refreshToken;
+
+    private LocalDateTime tokenExpirationTime;
+}
---
+++ src/main/java/com/app/global/jwt/dto/JwtTokenDto.java
@@ -0,0 +1,4 @@
+package com.app.global.jwt.dto;
+
+public class JwtTokenDto {
+}
